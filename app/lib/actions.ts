a1807abacb94c421f49e1e9eb5a9be670a937405
Cameron Schroeder <cameron@fallbackai.com>
2023-11-01 13:13:14 +1000

    Chapter 14: Improving Accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,13 +24,21 @@ export async function createInvoice(formData: FormData) {
 		amount: formData.get("amount"),
 		status: formData.get("status")
 	});
+
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split("T")[0];
 
-	await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+	try {
+		await sql`
+			INSERT INTO invoices (customer_id, amount, status, date)
+			VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+		`;
+	} catch (error) {
+		console.error(error);
+		return {
+			message: "Database Error: Failed to Create Invoice."
+		};
+	}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
@@ -48,11 +56,18 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 	const amountInCents = amount * 100;
 
-	await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+	try {
+		await sql`
+			UPDATE invoices
+			SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+			WHERE id = ${id}
+		`;
+	} catch (error) {
+		console.error(error);
+		return {
+			message: "Database Error: Failed to Update Invoice."
+		};
+	}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
@@ -61,6 +76,13 @@ export async function updateInvoice(id: string, formData: FormData) {
 // Delete Invoice
 
 export async function deleteInvoice(id: string) {
-	await sql`DELETE FROM invoices WHERE id = ${id}`;
-	revalidatePath("/dashboard/invoices");
+	try {
+		await sql`DELETE FROM invoices WHERE id = ${id}`;
+		revalidatePath("/dashboard/invoices");
+	} catch (error) {
+		console.error(error);
+		return {
+			message: "Database Error: Failed to Delete Invoice."
+		};
+	}
 }
